5d3e3c1be255fc03dd56fcb3a49e7ee4764a4ed6
johrielrsrd <johrielrsrd@gmail.com>
2023-10-08 21:47:23 +0800

    added map function and arrow es6 function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteDetails) => (
+        <Note
+          key={noteDetails.key}
+          title={noteDetails.title}
+          content={noteDetails.content}
+        />
+      ))}
       <Footer />
     </div>
   );
